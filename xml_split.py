bb93aa5bcc78ab3648411ffcea4ac2ce3ade955d
California Historical Society <blevay@calhist.org>
2017-03-15 12:24:35 -0700

    Added XML cleanup functions
    Cleans source XML by removing empty nodes, tags with "null" as text or
    attribute, etc.

--- xml_split.py
+++ xml_split.py
@@ -1,24 +1,48 @@
 # Split XML containing many <mods> elements into invidual files
 # Modified from script found here: http://stackoverflow.com/questions/36155049/splitting-xml-file-into-multiple-at-given-tags
 
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
+import lxml.etree as ET
 
-# register the namespaces
-ET.register_namespace('', "http://www.loc.gov/mods/v3")
-ET.register_namespace('xsi', "http://www.w3.org/2001/XMLSchema-instance")
-ET.register_namespace('xsi:schemaLocation', "http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-4.xsd")
+# parse source.xml with lxml
+tree = ET.parse('source.xml')
 
-# parse
-context = ET.iterparse('source.xml', events=('end', ))
+# remove empty nodes
+for element in tree.xpath(".//*[not(node())]"):
+	element.getparent().remove(element)
 
+# remove nodes with text "null"
+for element in tree.xpath(".//*[text()='null']"):
+	element.getparent().remove(element)
+
+# remove nodes with attribute "null"
+for element in tree.xpath(".//*[@*='null']"):
+	element.getparent().remove(element)
+
+# remove non-nodes
+for element in tree.iter():
+	if element.tag == '{http://www.loc.gov/mods/v3}subject':
+		element.tail = ''
+
+print "XML is now clean"
+
+# write to new xml
+tree.write('clean.xml')
+
+# parse the clean xml
+context = ET.iterparse('clean.xml', events=('end', ))
+
+# find the mods nodes
 for event, elem in context:
     if elem.tag == '{http://www.loc.gov/mods/v3}mods':
 
         # the filenames of the resulting xml files will be based on the <identifier> element
         title = elem.find('{http://www.loc.gov/mods/v3}identifier').text
         filename = format(title + ".xml")
+
+        # open a new file
         with open(filename, 'wb') as f:
             f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-            f.write(ET.tostring(elem))
+            f.write(ET.tostring(elem, pretty_print = True))
         print "Writing", filename
 print "All done"
